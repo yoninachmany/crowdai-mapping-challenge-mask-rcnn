395e8bd264aee95f13242e2e6ccaf183f3a425d8
root <root@tensorflow-2-vm.us-west1-b.c.dymaxion-labs-rooftop-counting.internal>
2019-01-26 16:44:02 +0000

    max_queue_size=1 not 100

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2379,7 +2379,7 @@ class MaskRCNN():
             callbacks=callbacks,
             validation_data=val_generator,
             validation_steps=self.config.VALIDATION_STEPS,
-            max_queue_size=100,
+            max_queue_size=1,
             workers=workers,
             use_multiprocessing=True,
         )
